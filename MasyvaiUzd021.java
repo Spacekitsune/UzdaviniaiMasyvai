b4be883e75316a44665dfa630df2002989d9f1c9
Spacekitsune <70965432+Spacekitsune@users.noreply.github.com>
2021-01-15 17:46:25 +0200

    update

--- MasyvaiUzd021.java
+++ MasyvaiUzd021.java
@@ -5,5 +5,35 @@ package UzdaviniaiMasyvai;
 //Apskaičiuokite kiek studentų neišlaikė egzamino ir koks yra pažangių studentų pažymių vidurkis.
 //Jei  studentas už egzaminą gavo pažymį 4 ar didesnį – jis pažangus.
 
+import java.util.Arrays;
 public class MasyvaiUzd021 {
+    public static void main(String[] args) {
+
+        int n = 30;
+        int[] pazymis = new int[n];
+
+        for (int i=0; i< pazymis.length;i++) {
+            pazymis[i]=getRandom(1,10);
+        }
+
+        System.out.println(Arrays.toString(pazymis));
+
+        int countPazangus=0;
+        int countFail=0;
+
+        for (int i=0; i<pazymis.length ;i++){
+            if (pazymis[i]>3) {
+                countPazangus++;
+            } else {
+                countFail++;
+            }
+        }
+
+        System.out.printf("%d mokinių yra pažangūs, %d neišlaikė egzamino.", countPazangus, countFail);
+
+    }
+    private static int getRandom(int a, int b) {
+        return (a + (int) (Math.random() * ((b - a) + 1)));
+
+    }
 }
--- MasyvaiUzd022.java
+++ MasyvaiUzd022.java
@@ -10,5 +10,53 @@ package UzdaviniaiMasyvai;
 //Trečioje eilutėje – neigiamų elementų kiekis
 //Jei nėra neigiamų elementų programos rezultatas žodis NE.
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class MasyvaiUzd022 {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        int n = 20;
+        int[] arrN = new int[n];
+
+        System.out.print("Iveskite intervalo pradžios skaičių: ");
+        int a = reader.nextInt();
+        System.out.print("Iveskite intervalo pabaigos skaičių: ");
+        int b = reader.nextInt();
+
+        int counter = 0;
+
+        for (int i = 0; i < n; i++) {
+            arrN[i] = getRandom(a, b);
+            if (arrN[i] < 0) {
+                counter++;
+            }
+        }
+
+        int[] negative = new int[counter];
+
+        for (int i = 0; i < negative.length; i++) {
+
+            for (int j = 0; j < arrN.length; j++) {
+                if (arrN[j] < 0) {
+                    negative[i]=arrN[j];
+                    i++;
+                }
+            }
+        }
+
+        if (counter==0) {
+            System.out.println("NE.");
+        } else {
+            System.out.println("Taip.");
+            System.out.println(Arrays.toString(negative));
+            System.out.printf("Neigiamų elementų kiekis: %d.", counter);
+        }
+
+        reader.close();
+    }
+    private static int getRandom(int a, int b) {
+        return (a + (int) (Math.random() * ((b - a) + 1)));
+
+    }
 }
